356676dcd5dd7779a4890ae896d104dd59caa590
Phan Huy <fvn.huypd@gmail.com>
2017-11-06 19:09:31 +0700

    part 3

--- js/common.js
+++ js/common.js
@@ -1,158 +1,229 @@
 $(document).ready(function() {
-	"use strict";
-	$('#menu-toggle').click(function() {
-		$(this).toggleClass('active');
-		$('#header .container_right').toggleClass('open');
-	});
-});
-
-$(window).bind('load resize', function() {
-	"use strict";
-	if( $('#page-top').length === 0 ) {
-		var header_h = $('#header').outerHeight();
-		$('#main').css('margin-top', header_h);
-	} else {
-		$('#main').css('margin-top', 0);
-	}
+    "use strict";
+    $('#menu-toggle').click(function() {
+        $(this).toggleClass('active');
+        $('#header .container_right').toggleClass('open');
+    });
 });
 
 $(document).ready(function() {
     "use strict";
-    if($('#candosoon_slider').length) {
-    	$('#candosoon_slider').slick({
-	        dots: false,
-	        infinite: true,
-	        speed: 800,
-	        slidesToShow: 4,
-	        slidesToScroll: 1,
-	        autoplay: true,
-	        autoplaySpeed: 3000,
-	        arrows: true,
-	        pauseOnHover: false,
-	        responsive: [{
-	                breakpoint: 900,
-	                settings: {
-	                    slidesToShow: 3,
-	                }
-	            },
-	            {
-	                breakpoint: 641,
-	                settings: {
-	                    slidesToShow: 2,
-	                }
-	            },
-	            {
-	                breakpoint: 461,
-	                settings: {
-	                    slidesToShow: 1,
-	                }
-	            }
-	        ]
-	    });
+    if ($('#candosoon_slider').length) {
+        $('#candosoon_slider').slick({
+            dots: false,
+            infinite: true,
+            speed: 800,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: true,
+            pauseOnHover: false,
+            responsive: [{
+                    breakpoint: 900,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 641,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 461,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+            ]
+        });
     }
-    if($('#campaign_slider').length) {
-    	$('#campaign_slider').slick({
-	        dots: false,
-	        infinite: true,
-	        speed: 800,
-	        slidesToShow: 4,
-	        slidesToScroll: 1,
-	        autoplay: true,
-	        autoplaySpeed: 3000,
-	        arrows: true,
-	        pauseOnHover: false,
-	        responsive: [{
-	                breakpoint: 900,
-	                settings: {
-	                    slidesToShow: 3,
-	                }
-	            },
-	            {
-	                breakpoint: 641,
-	                settings: {
-	                    slidesToShow: 2,
-	                }
-	            },
-	            {
-	                breakpoint: 461,
-	                settings: {
-	                    slidesToShow: 1,
-	                }
-	            }
-	        ]
-	    });
+    if ($('#campaign_slider').length) {
+        $('#campaign_slider').slick({
+            dots: false,
+            infinite: true,
+            speed: 800,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: true,
+            pauseOnHover: false,
+            responsive: [{
+                    breakpoint: 900,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 641,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 461,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+            ]
+        });
     }
-    if($('#feature_slider').length) {
-    	$('#feature_slider').slick({
-	        dots: false,
-	        infinite: true,
-	        speed: 800,
-	        slidesToShow: 3,
-	        slidesToScroll: 1,
-	        autoplay: true,
-	        autoplaySpeed: 3000,
-	        arrows: true,
-	        pauseOnHover: false,
-	        responsive: [{
-	                breakpoint: 900,
-	                settings: {
-	                    slidesToShow: 2,
-	                }
-	            },
-	            {
-	                breakpoint: 641,
-	                settings: {
-	                    slidesToShow: 1,
-	                }
-	            }
-	        ]
-	    });
+    if ($('#feature_slider').length) {
+        $('#feature_slider').slick({
+            dots: false,
+            infinite: true,
+            speed: 800,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: true,
+            pauseOnHover: false,
+            responsive: [{
+                    breakpoint: 900,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 641,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+            ]
+        });
     }
 
-    if(($(window).width() <= 1024) && ($('#popular_search').length) ) {
+    if (($(window).width() <= 1024) && ($('#popular_search').length)) {
         $('#popular_search .box_tab .box_items').slick({
-	        dots: false,
-	        infinite: true,
-	        speed: 800,
-	        slidesToShow: 4,
-	        slidesToScroll: 1,
-	        autoplay: true,
-	        autoplaySpeed: 3000,
-	        arrows: true,
-	        pauseOnHover: false,
-	        responsive: [
-	            {
-	                breakpoint: 641,
-	                settings: {
-	                    slidesToShow: 3,
-	                }
-	            },
-	            {
-	                breakpoint: 421,
-	                settings: {
-	                    slidesToShow: 2,
-	                }
-	            },
-	            {
-	                breakpoint: 361,
-	                settings: {
-	                    slidesToShow: 1,
-	                }
-	            }
-	        ]
-	    });
+            dots: false,
+            infinite: true,
+            speed: 800,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: true,
+            pauseOnHover: false,
+            responsive: [{
+                    breakpoint: 641,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 421,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 361,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+            ]
+        });
+    }
+
+    if ($('#lession').length) {
+        $('#lession .content').slick({
+            dots: false,
+            infinite: true,
+            speed: 800,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: true,
+            pauseOnHover: false,
+            responsive: [{
+                    breakpoint: 1201,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 851,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                },
+                {
+                    breakpoint: 769,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 641,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+            ]
+        });
     }
 });
 
 
-$(window).bind('load scroll', function() {
+$(document).ready(function() {
     "use strict";
-    if($('#page-top').length) {
-        var position = $('#top_info').outerHeight();
-        if($(this).scrollTop() >= position) {
-            $('#header').removeClass('menu_top');
-        } else {
-            $('#header').addClass('menu_top');
-        }
+    if ($('.breadcrumbs').length) {
+        var breadcrumb_pos = $('.breadcrumbs').offset().top;
+        var breadcrumb_h = $('.breadcrumbs').outerHeight();
+        $(window).bind('load scroll', function() {
+            if ($(this).scrollTop() >= (breadcrumb_pos)) {
+                $('.breadcrumbs').addClass('fixed');
+                $('.breadcrumbs').css({
+                    'position': 'fixed',
+                });
+                $('.breadcrumbs').next().css('margin-top', breadcrumb_h);
+            } else {
+                $('.breadcrumbs').removeClass('fixed');
+                $('.breadcrumbs').css({
+                    'position': 'static',
+                });
+                $('.breadcrumbs').next().css('margin-top', 0);
+            }
+        });
     }
+});
+
+
+
+$(document).ready(function() {
+    "use strict";
+    $('.popup .btn_close').click(function() {
+        $(this).parents('.popup').fadeOut();
+    });
+
+    $('#lession .content .post').click(function() {
+        $(this).parents('#lession').find('.popup').fadeIn();
+    });
+
+    $('#rental_coat .book_info .detail .content ul li').click(function() {
+        $(this).parents('#rental_coat').find('.popup').fadeIn();
+    });
+});
+
+
+
+$(document).ready(function() {
+	"use strict";
+	$('#header .container_right .btn_letter').click(function() {
+		$('#request_setting').fadeIn();
+	});
+	$('#request_setting .submit').click(function() {
+		$(this).parents('#request_setting').fadeOut();
+	});
+	$('#request_setting .overlay').click(function() {
+		$(this).parents('#request_setting').fadeOut();
+	});
 });
