004710c7cf4bf6c62236164b0c3789379c3a6bbd
Phan Huy <fvn.huypd@gmail.com>
2017-11-08 19:04:57 +0700

    part 5

--- js/common.js
+++ js/common.js
@@ -1,15 +1,12 @@
-//menu icon
 $(document).ready(function() {
     "use strict";
+    //menu icon
     $('#menu-toggle').click(function() {
         $(this).toggleClass('active');
         $('#header .container_right').toggleClass('open');
     });
-});
 
-//slider
-$(document).ready(function() {
-    "use strict";
+	//slider
     if ($('#candosoon_slider').length) {
         $('#candosoon_slider').slick({
             dots: false,
@@ -203,11 +200,8 @@ $(document).ready(function() {
         });
     }
 
-});
 
-//fixed breadcrumbs
-$(document).ready(function() {
-    "use strict";
+    //fixed breadcrumbs
     if ($('.breadcrumbs').length) {
         var breadcrumb_pos = $('.breadcrumbs').offset().top;
         var breadcrumb_h = $('.breadcrumbs').outerHeight();
@@ -227,12 +221,9 @@ $(document).ready(function() {
             }
         });
     }
-});
 
 
-//popup
-$(document).ready(function() {
-    "use strict";
+    //popup
     $('.popup .btn_close').click(function() {
         $(this).parents('.popup').fadeOut();
     });
@@ -241,15 +232,12 @@ $(document).ready(function() {
         $(this).parents('#lession').find('.popup').fadeIn();
     });
 
-    $('#rental_coat .book_info .detail .content ul li').click(function() {
+    $('#rental_coat .book_info .detail .content ul li:nth-child(2), #rental_coat .book_info .detail .content ul li:nth-child(3)').click(function() {
         $(this).parents('#rental_coat').find('.popup').fadeIn();
     });
-});
 
 
-//form request_setting
-$(document).ready(function() {
-    "use strict";
+    //form request_setting
     $('#header .container_right .btn_letter').click(function() {
         $('#request_setting').fadeIn();
     });
@@ -259,11 +247,8 @@ $(document).ready(function() {
     $('#request_setting .overlay').click(function() {
         $(this).parents('#request_setting').fadeOut();
     });
-});
 
-//tab index
-$(document).ready(function() {
-    "use strict";
+    //tab index
     $('#popular_search li').click(function() {
         if ($(this).hasClass('active') === false) {
             $(this).parent().find('.active').removeClass('active');
@@ -275,11 +260,118 @@ $(document).ready(function() {
         }
     });
 
+	//change menu color
+	$('#form_search_header ul li').click(function() {
+		if($(this).hasClass('active') === false) {
+			$(this).parent().find('.active').removeClass('active');
+			$(this).addClass('active');
+
+			if($(this).attr('id') === "for_court") {
+				$('body').removeClass('content_lession');
+				$('body').addClass('content_court');
+				var placeholder = $('#input_search').attr('data-for-court');
+				$('#input_search').attr('placeholder', placeholder);
+			} else if($(this).attr('id') === "for_lession") {
+				$('body').removeClass('content_court');
+				$('body').addClass('content_lession');
+				var placeholder = $('#input_search').attr('data-for-lession');
+				$('#input_search').attr('placeholder', placeholder);
+			}
+		}
+	});
+
+	//add class body
+	if($('body').attr('data-is-page') === 'court') {
+		$('body').addClass('content_court');
+		$('#form_search_header ul li#for_court').trigger('click');
+	}
+
+	//icon message
+	$('#main .btn_letter').click(function() {
+		$(this).toggleClass('active');
+	});
+
+	$('#main .btn_star').click(function() {
+		$(this).find('i').toggleClass('fa-star-o');
+		$(this).find('i').toggleClass('fa-star');
+	});
+
+
+	$('.controls ul li').click(function() {
+		$(this).toggleClass('active');
+	});
+
+	$('#rental_coat .surface li').click(function() {
+		$(this).toggleClass('active');
+	});
+
+	if ($('.court_item .box_img .list_img ul').length) {
+        $('.court_item .box_img .list_img ul').slick({
+            dots: false,
+            infinite: true,
+            speed: 800,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: true,
+            pauseOnHover: false,
+        });
+    }
+    if ($('#page-search-list #posts_container .post .thumbs .images').length) {
+        $('#page-search-list #posts_container .post .thumbs .images').slick({
+            dots: false,
+            infinite: true,
+            speed: 800,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: true,
+            pauseOnHover: false,
+        });
+    }
+
+
+
+    $('.item_get_link').click(function() {
+    	var link = $(this).find('a').attr('href');
+    	window.location.assign(link);
+    });
+
+    
+    $('.list_option_search .box_more .more_option li input').on('change', function() {
+    	var flag = 0;
+    	$(this).parents('.more_option').find('li').each(function() {
+    		if($(this).find('input').prop('checked') === true) {
+    			flag++;
+    		}
+    	});
+    	if(flag !== 0) {
+    		$('.list_option_search .box_more > li').addClass('active');
+    	} else {
+    		$('.list_option_search .box_more > li').removeClass('active');
+    	}
+    });
+
+    $('.list_option_search .box_surface input').on('change', function() {
+    	var flag = 0;
+    	$(this).parents('.box_surface').find('li').each(function() {
+    		if($(this).find('input').prop('checked') === true) {
+    			flag++;
+    		}
+    	});
+    	if(flag !== 0) {
+    		$('.list_option_search .box_surface > li').addClass('active');
+    	} else {
+    		$('.list_option_search .box_surface > li').removeClass('active');
+    	}
+    });
+
+
 });
 
-$(document).ready(function() {
+
+$(window).bind('load resize', function() {
 	"use strict";
-	setTimeout(function() {
-		// $('#popular_search .box_tab #tab_lession');
-	}, 500);
 });
